4d538a6e819b4ea5353654dc8c704effa4e61abd
Luciana Solorzano <luciana@brightvessel.com>
2024-11-18 13:06:27 -0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+console.log("JavaScript loaded")
+
+window.addEventListener("load", function() {
+    const mainMenu = document.getElementById("main-menu");
+    mainMenu.classList.add('menu-slide-in');
+  });
+
+
+window.addEventListener("load", function() {
+    const mainMenu = document.getElementById("logo");
+    mainMenu.classList.add('fade-in');
+});
